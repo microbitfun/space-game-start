5622c7c5b0a0049d282a33d877f0bc2099a181a9
MicrobitFun <68689935+microbitfun@users.noreply.github.com>
2020-08-10 18:34:44 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+input.onPinPressed(TouchPin.P0, function () {
+    ship.change(LedSpriteProperty.X, -1)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    ship.change(LedSpriteProperty.X, 1)
+})
+let enemy: game.LedSprite = null
+let ship: game.LedSprite = null
+ship = game.createSprite(2, 4)
+ship.set(LedSpriteProperty.Brightness, 250)
+game.setScore(0)
+basic.forever(function () {
+    enemy = game.createSprite(randint(0, 4), 0)
+    enemy.set(LedSpriteProperty.Brightness, 150)
+    basic.pause(150)
+    enemy.turn(Direction.Right, 90)
+    for (let index = 0; index < 4; index++) {
+        enemy.change(LedSpriteProperty.Y, 1)
+        basic.pause(500)
+        if (enemy.isTouching(ship)) {
+            game.gameOver()
+        }
+    }
+    if (enemy.isTouchingEdge()) {
+        enemy.delete()
+    }
+})
